914532b9686ac7c587eda185e127c75cc29df73d
Srivatsav, Gunisetty <srivatsav.gunisetty@philips.com>
2020-08-27 11:59:24 +0530

    Implement abstraction

--- checker.cpp
+++ checker.cpp
@@ -1,28 +1,22 @@
 #include <assert.h>
+#include <iostream>
 
-// If anything is okay return 0 else, return non-zero
-
-int bpm_ok(float bpm){
-  return !(bpm < 70 || bpm > 150);
-}
-
-int spo2_ok(float spo2){
-  return !(spo2 < 90);
-}
-
-int respRate_ok(float respRate){
-  return !(respRate < 30 || respRate > 95);
-}
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  int val = bpm_ok(bpm) | spo2_ok(spo2) | respRate_ok(respRate);
-  if(val == 0){
+bool isInsideLimit(float value, float lowerLimit, float upperLimit){
+  if( value >= lowerLimit && value <= upperLimit){
     return true;
   }
   return false;
 }
 
+void testAbstractFunction(){
+  assert(isInsideLimit(10, 30, 40) == false);
+  assert(isInsideLimit(35, 30, 40) == true);
+  assert(isInsideLimit(45, 30, 40) == false);
+  assert(isInsideLimit(30, 30, 40) == true);
+  assert(isInsideLimit(40, 30, 40) == true);
+}
+
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+  testAbstractFunction();
+  std::cout << "All tests passed!!" << std::endl;
 }
