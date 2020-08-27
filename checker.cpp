ed81759a1adc121700def16cb07801aadf4e6bd4
Srivatsav, Gunisetty <srivatsav.gunisetty@philips.com>
2020-08-27 11:09:03 +0530

    reduce CCN

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,25 @@
 #include <assert.h>
 
+// If anything is okay return 0 else, return non-zero
+
+int bpm_ok(float bpm){
+  return !(bpm < 70 || bpm > 150);
+}
+
+int spo2_ok(float spo2){
+  return !(spo2 < 90);
+}
+
+int respRate_ok(float respRate){
+  return !(respRate < 30 || respRate > 95);
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
+  int val = bpm_ok(bpm) | spo2_ok(spo2) | respRate_ok(respRate);
+  if(val == 0){
+    return true;
   }
-  return true;
+  return false;
 }
 
 int main() {
